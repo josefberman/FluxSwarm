f99f75d7d049a6356e5ed29d3e4cb4f12aad32b8
Assaf-Berman <assafberman@outlook.com>
2024-08-31 17:11:47 +0300

    sphere flow - cm

--- main.py
+++ main.py
@@ -7,23 +7,22 @@ from phi.flow import *
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from datetime import datetime
-import matplotlib.colors as colors
 
 # -------------- Parameter Definition -------------
 length_x = 40  # cm
 length_y = 3.2  # cm
-resolution = (4000, 320)
-dx = length_x / resolution[0]  # cm
-dy = length_y / resolution[1]  # cm
+resolution = (400, 320)
+dx = length_x / resolution[0]
+dy = length_y / resolution[1]
 swarm_num_x = 5
 swarm_num_y = 5
-swarm_member_rad = 0.16  # cm
+swarm_member_rad = 0.04
 inflow_freq = 1  # Hz
-inflow_amplitude = 30  # cm/s
+inflow_amplitude = 0.1  # cm/s
 inflow_center_x = 50 * dx
 inflow_center_y = length_y / 2
 inflow_radius = 20 * dy
-viscosity = 1e-03  # Pa*s
+viscosity = 0.0089  # dyne*s/cm^2
 dt = 0.05  # s
 total_time = 50  # s
 
@@ -33,59 +32,41 @@ box = Box['x,y', 0:length_x, 0:length_y]
 # -------------- Swarm Generation ------------------
 swarm = []
 
-for i in np.linspace(35, 38, swarm_num_x):
-    for j in np.linspace(length_y / (swarm_num_y * 2), length_y - length_y / (swarm_num_y * 2), swarm_num_y):
-        swarm.append(Obstacle(Sphere(x=i, y=j, radius=swarm_member_rad)))
+# for i in np.linspace(2, 3, swarm_num_x):
+#     for j in np.linspace(0.1, 0.9, swarm_num_y):
+#         swarm.append(Obstacle(Sphere(x=i, y=j, radius=swarm_member_rad)))
+
+
+# ---- initial u and p vector field Generation ----
+boundary = {'x': ZERO_GRADIENT, 'y': 0}
+velocity = StaggeredGrid(0, boundary=boundary, bounds=box, x=resolution[0], y=resolution[1])
+inflow_sphere = Sphere(x=inflow_center_x, y=inflow_center_y, radius=inflow_radius)
+inflow = CenteredGrid(0, boundary=boundary, bounds=box, x=resolution[0], y=resolution[1])
 
 
 # -------------- Step Definition -------------------
-def step(velocity_prev, inflow, inflow_amplitude, inflow_freq, dt, t):
-    # inflow = advect.mac_cormack(inflow, velocity_prev, dt)
-    velocity_tent = advect.semi_lagrangian(velocity_prev, velocity_prev, dt) + inflow_amplitude * inflow
-    # inflow_component = (inflow_amplitude * 0.5 * math.cos(inflow_freq * math.pi * t) + 0.5) * inflow * dt
-    velocity_tent = diffuse.explicit(velocity_tent, viscosity, dt, substeps=10_000)
-    velocity_next, pressure = fluid.make_incompressible(velocity_tent, swarm,
-                                                        Solve(rel_tol=1e-05, abs_tol=1e-05, max_iterations=100_000))
-    return velocity_next, pressure, inflow
+def step(v, inflow, p, dt):
+    inflow = advect.mac_cormack(inflow, v, dt) + inflow_amplitude * resample(inflow_sphere, to=inflow, soft=True)
+    inflow_velocity = resample(inflow * (1, 0), to=v)
+    v = advect.semi_lagrangian(v, v, dt) + inflow_velocity * dt
+    v, p = fluid.make_incompressible(v, swarm, Solve(rel_tol=1e-03, abs_tol=1e-03, x0=p, max_iterations=10_000))
+    return v, p, inflow
 
 
 # ------------- Plotting functions ----------------
 def plot_scalar_field_with_patches(field, box, ax, title):
     max_magnitude = np.max(np.abs(field.numpy()))
     im = ax.imshow(field.numpy().T, origin='lower', cmap='coolwarm_r', extent=[0, length_x, 0, length_y], aspect=4,
-                   norm=colors.SymLogNorm(linthresh=0.03, linscale=0.03, vmin=-max_magnitude, vmax=max_magnitude))
-    lower = box.lower.numpy()
-    upper = box.upper.numpy()
-    width = upper[0] - lower[0]
-    height = upper[1] - lower[1]
-    rect = patches.Rectangle(lower, width, height, linewidth=0, facecolor='white')
-    ax.add_patch(rect)
+                   vmin=-max_magnitude, vmax=max_magnitude)
+    # lower = box.lower.numpy()
+    # upper = box.upper.numpy()
+    # width = upper[0] - lower[0]
+    # height = upper[1] - lower[1]
+    # rect = patches.Rectangle(lower, width, height, linewidth=0, facecolor='white')
+    # ax.add_patch(rect)
     ax.set_title(title)
     return im
 
-
-# ---- initial u and p vector field Generation ----
-velocity_boundaries = extrapolation.combine_sides({'x-': extrapolation.BOUNDARY, 'x+': extrapolation.BOUNDARY,
-                                                   'y+': extrapolation.ZERO, 'y-': extrapolation.ZERO})
-velocity = StaggeredGrid(0, extrapolation=velocity_boundaries, bounds=box, x=resolution[0], y=resolution[1])
-inflow_grid = CenteredGrid(0.0, extrapolation=velocity_boundaries, bounds=box, x=resolution[0], y=resolution[1])
-pressure_box = Box['x,y', 50 * dx:100 * dx, 50 * dy:length_y - 49 * dy]
-pressure_box_grid = pressure_box @ inflow_grid
-# pressure_box = Sphere(x=inflow_center_x, y=inflow_center_y, radius=inflow_radius) @ inflow_grid
-# pressure_box_u = pressure_box.numpy().astype(float)
-# pressure_box_v = pressure_box.numpy().astype(float)
-# for index in np.transpose(np.nonzero(pressure_box_u)):
-#     pressure_box_u[index[0], index[1]] = (index[0] - inflow_center_x / dx) / inflow_radius
-#     pressure_box_v[index[0], index[1]] = (index[1] - inflow_center_y / dy) / inflow_radius
-# pressure_box_u = math.tensor(pressure_box_u, spatial('x,y'))
-# pressure_box_v = math.tensor(pressure_box_v, spatial('x,y'))
-# pressure_box = math.stack([pressure_box_u, pressure_box_v], channel('vector'))
-inflow = StaggeredGrid(pressure_box_grid * vec(x=1, y=0), extrapolation=velocity_boundaries, bounds=box,
-                       x=resolution[0],
-                       y=resolution[1])
-vis.plot(inflow['x'], inflow['y'])
-plt.show()
-velocity, pressure = fluid.make_incompressible(velocity=velocity, obstacles=swarm)
 
 # ----------------- Calculation --------------------
 folder_name = f'{datetime.now().year}-{datetime.now().month}-{datetime.now().day}_{datetime.now().hour}-{datetime.now().minute}-{datetime.now().second}'
@@ -109,7 +90,7 @@ with open(f'./run_{folder_name}/configuration.txt', 'w') as f:
 for time_step in range(total_time):
     print('Time Step:', time_step * dt)
     calc_start = datetime.now()
-    velocity, pressure, inflow = step(velocity, inflow, inflow_amplitude, inflow_freq, dt, time_step * dt)
+    velocity, pressure, inflow = step(velocity, inflow, None, dt)
     print('Calculation time:', datetime.now() - calc_start)
     fig, axes = plt.subplots(3, 1, figsize=(10, 20))
     fields = [velocity['x'], velocity['y'], pressure]
@@ -131,3 +112,4 @@ for time_step in range(total_time):
     plt.close(fig)
     phi.field.write(velocity, f'./run_{folder_name}/velocity/{time_step * dt:.3f}')
     phi.field.write(pressure, f'./run_{folder_name}/pressure/{time_step * dt:.3f}')
+

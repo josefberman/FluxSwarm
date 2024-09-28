29497427a35cb0e78e63949a42f6856ee9e58afc
Assaf-Berman <assafberman@outlook.com>
2024-09-28 18:55:42 +0300

    Organized project

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from simulation import run_simulation
 
 # -------------- Parameter Definition -------------
 # Simulation dimensions are μm and second
-sim = Simulation(length_x=300, length_y=10, resolution=(1500, 50), dt=0.1, total_time=10)
-swarm = Swarm(num_x=5, num_y=5, member_radius=0.04)
-inflow = Inflow(frequency=2, amplitude=1000, radius=sim.length_y / 2, center_y=sim.length_y / 2)
+sim = Simulation(length_x=18000, length_y=800, resolution=(1500, 50), dt=0.1, total_time=10)
+swarm = Swarm(num_x=5, num_y=5, member_radius=50)
+inflow = Inflow(frequency=2*np.pi, amplitude=5940, radius=sim.length_y / 2, center_y=sim.length_y / 2)
 #inflow.center_x = inflow.radius + sim.dx
 inflow.center_x = 0
 fluid = Fluid(viscosity=0.0089)

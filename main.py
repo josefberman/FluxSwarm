b6afb7fe52a7b82c48e3340b849132a331c0a885
Assaf <assafberman@outlook.com>
2025-07-06 18:51:07 +0300

    Longer simulation. one instance

--- main.py
+++ main.py
@@ -21,15 +21,32 @@ assert backend.default_backend().set_default_device('GPU')
 
 # spla.use_solver(useUmfpack=True)
 
+
 def main():
-    for iter_max_force in range(10,101,10):
+    for iter_max_force in range(10, 101, 10):
+        print('Max force:', iter_max_force)
         # -------------- Parameter Definition -------------
         # Simulation dimensions are length=mm and time=second, mass=mg
-        sim = Simulation(length_x=100, length_y=4, resolution=(2500, 100), dt=0.05, total_time=2000)
-        swarm = Swarm(num_x=3, num_y=3, left_location=49, bottom_location=1, member_interval_x=1, member_interval_y=1,
-                    member_radius=0.1, member_density=5.150, member_max_force=iter_max_force)  # density in mg/mm^3, force in mg*mm/s^2
+        sim = Simulation(length_x=100,
+                         length_y=4,
+                         resolution=(2500, 100),
+                         dt=0.05,
+                         total_time=5000)
+        swarm = Swarm(num_x=3,
+                      num_y=3,
+                      left_location=49,
+                      bottom_location=1,
+                      member_interval_x=1,
+                      member_interval_y=1,
+                      member_radius=0.1,
+                      member_density=5.150,
+                      member_max_force=iter_max_force
+                      )  # density in mg/mm^3, force in mg*mm/s^2
         # max force 0.017 mg*mm/s^2
-        inflow = Inflow(frequency=2 * np.pi, amplitude=200, radius=sim.length_y / 2, center_y=sim.length_y / 2)
+        inflow = Inflow(frequency=2 * np.pi,
+                        amplitude=200,
+                        radius=sim.length_y / 2,
+                        center_y=sim.length_y / 2)
         inflow.center_x = 0
         fluid = Fluid(viscosity=3.0)  # viscosity of blood in mg/(mm*s)
 
@@ -38,12 +55,20 @@ def main():
 
         # ---- initial v and p Vector Field Generation ----
         boundary = {'x': ZERO_GRADIENT, 'y': 0}
-        velocity_field = StaggeredGrid(0, boundary=boundary, bounds=box, x=sim.resolution[0], y=sim.resolution[1])
+        velocity_field = StaggeredGrid(0,
+                                       boundary=boundary,
+                                       bounds=box,
+                                       x=sim.resolution[0],
+                                       y=sim.resolution[1])
 
         # ----------------- Calculation --------------------
         folder_name = create_run_name()
         create_folders_for_run(folder_name)
-        log_parameters(folder_name=folder_name, sim=sim, swarm=swarm, inflow=inflow, fluid=fluid)
+        log_parameters(folder_name=folder_name,
+                       sim=sim,
+                       swarm=swarm,
+                       inflow=inflow,
+                       fluid=fluid)
 
         # run_simulation(velocity_field=velocity_field, pressure_field=None, inflow=inflow, sim=sim,
         #                swarm=swarm, fluid_obj=fluid, folder_name=folder_name)
@@ -62,8 +87,14 @@ def main():
         # ----------- Reinforcement Learning - PPO ------------------
 
         os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
         def make_env():
-            return SwarmEnv(sim=sim, swarm=swarm, fluid=fluid, inflow=inflow, folder=folder_name)
+            return SwarmEnv(sim=sim,
+                            swarm=swarm,
+                            fluid=fluid,
+                            inflow=inflow,
+                            folder=folder_name)
+
         # env = SwarmEnv(sim=sim, swarm=swarm, fluid=fluid, inflow=inflow, folder=folder_name)
         num_envs = 1
         env = SubprocVecEnv([make_env for _ in range(num_envs)])
@@ -78,9 +109,10 @@ def main():
 
         # ----------------- Animation --------------------
         # for env_i in range(env.num_envs):
-        #     animate_save_simulation(sim=env.get_attr('sim')[env_i], swarm=env.get_attr('swarm')[env_i], 
+        #     animate_save_simulation(sim=env.get_attr('sim')[env_i], swarm=env.get_attr('swarm')[env_i],
         #                             folder_name=f'{env.get_attr('folder')[env_i]}/PPO/{env_i}', inflow=env.get_attr('inflow')[env_i])
         # animate_save_simulation(sim=env.sim, swarm=env.swarm, folder_name=env.folder, inflow=env.inflow)
+
 
 if __name__ == '__main__':
     from multiprocessing import freeze_support
